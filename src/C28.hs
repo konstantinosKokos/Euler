51158a17135126ed570e156d7aea126cb2620e60
konstantinosKokos <konstantinos@riseup.net>
2020-09-28 15:29:33 +0200

    28

---
+++ src/C28.hs
@@ -0,0 +1,13 @@
+module C28
+  (result)
+  where
+
+import Data.Array
+
+takeEvery :: Int -> Int -> [Int] -> [Int]
+takeEvery _ _ [] = []
+takeEvery d 0 (x1:x2:xs) = takeEvery (d+2) 4 xs
+takeEvery d m (x:xs) = x:takeEvery d (m-1) (drop (d-1) xs)
+
+
+result = sum (takeEvery 2 4 [3..1001^2]) + 1
